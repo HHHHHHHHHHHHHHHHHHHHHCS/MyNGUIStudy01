4f69330e6f5227e4af0e7465452b20c134d8f663
HCS <464962683@qq.com>
2018-04-21 22:18:26 +0800

    SPRITE  BUTTOBN  PANEL  ALTLAS 复习

---
+++ Assets/My/Scripts/NGUIButtonClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NGUIButtonClick : MonoBehaviour
+{
+    public GameObject labelButton;
+    public GameObject spriteButton;
+    public UIButton numberButton;
+
+
+    private void Awake()
+    {
+        UIEventListener.Get(labelButton).onClick = go => { Debug.Log("Click:"+go); };
+        UIEventListener.Get(spriteButton).onClick =go => { Debug.Log("Click:" + go); };
+        //numberButton.SetState(UIButtonColor.State.Disabled, true);
+        //numberButton.state = UIButtonColor.State.Disabled;
+        numberButton.GetComponent<BoxCollider>().enabled = false;
+    }
+}
