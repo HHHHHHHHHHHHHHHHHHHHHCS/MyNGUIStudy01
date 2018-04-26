236410136bd4d4b10fdbdf8a694a25c20e92b54f
HCS <464962683@qq.com>
2018-04-26 23:05:07 +0800

    打字机，彩色滑动条，按钮里面的文字自适应复习

---
+++ Assets/My/Scripts/GameMenu/GameSetting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameGrade
+{
+    Esay,
+    Normal,
+    Diffculty,
+}
+
+public enum ControlType
+{
+    Keyboard,
+    Touch,
+    Mouse,
+}
+
+
+
+public class GameSetting : MonoBehaviour
+{
+    public static float volume = 1;
+    public static GameGrade gameGrade = GameGrade.Esay;
+    public static ControlType controlType = ControlType.Keyboard;
+    public static bool isFullScreen = false;
+}
