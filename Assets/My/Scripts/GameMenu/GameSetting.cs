e3f730eb270f32741f111b73e7a469244ae920bc
HCS <464962683@qq.com>
2018-04-28 14:23:40 +0800

    下拉菜单 Toggle Slider 的事件添加

--- Assets/My/Scripts/GameMenu/GameSetting.cs
+++ Assets/My/Scripts/GameMenu/GameSetting.cs
@@ -6,7 +6,7 @@ public enum GameGrade
 {
     Esay,
     Normal,
-    Diffculty,
+    Hard,
 }
 
 public enum ControlType
@@ -20,8 +20,79 @@ public enum ControlType
 
 public class GameSetting : MonoBehaviour
 {
-    public static float volume = 1;
-    public static GameGrade gameGrade = GameGrade.Esay;
-    public static ControlType controlType = ControlType.Keyboard;
-    public static bool isFullScreen = false;
+    public  float volume = 1;
+    public  GameGrade gameGrade = GameGrade.Esay;
+    public  ControlType controlType = ControlType.Keyboard;
+    public  bool isFullScreen = false;
+
+
+    private UISlider volumeSlider;
+    private UIPopupList gameGradeList;
+    private UIPopupList gameControllerList;
+    private UIToggle isFullScreenToggle;
+
+    private void Awake()
+    {
+        volumeSlider = transform.Find("VolumeLabel/VolumeSlider").GetComponent<UISlider>();
+        gameGradeList = transform.Find("GameGradeLabel/GameGradeList").GetComponent<UIPopupList>();
+        gameControllerList = transform.Find("GameControllerLabel/GameControllerList").GetComponent<UIPopupList>();
+        isFullScreenToggle = transform.Find("IsFullScreenLabel/IsFullToggle").GetComponent<UIToggle>();
+
+
+        EventDelegate.Add(volumeSlider.onChange, OnVolumeSliderChanged);
+        EventDelegate.Add(gameGradeList.onChange, OnGameGradeChanged);
+        EventDelegate.Add(gameControllerList.onChange, OnGameControlChanged);
+        EventDelegate.Add(isFullScreenToggle.onChange, OnIsFullChanged);
+
+    }
+
+    private void OnVolumeSliderChanged()
+    {
+        Debug.Log("OnVolumeSliderChanged:" + UIProgressBar.current.value);
+        volume = UIProgressBar.current.value;
+    }
+
+    private void OnGameGradeChanged()
+    {
+        Debug.Log("OnGameGradeChanged:" + UIPopupList.current.value);
+        switch (UIPopupList.current.value)
+        {
+            case "简单":
+                gameGrade = GameGrade.Esay;
+                break;
+            case "普通":
+                gameGrade = GameGrade.Normal;
+                break;
+            case "困难":
+                gameGrade = GameGrade.Hard;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnGameControlChanged()
+    {
+        Debug.Log("OnGameControlChanged:" + UIPopupList.current.value);
+        switch (UIPopupList.current.value)
+        {
+            case "键盘":
+                controlType = ControlType.Keyboard;
+                break;
+            case "触摸":
+                controlType = ControlType.Touch;
+                break;
+            case "鼠标":
+                controlType = ControlType.Mouse;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnIsFullChanged()
+    {
+        Debug.Log("OnIsFullChanged:" + UIToggle.current.value);
+        isFullScreen = UIToggle.current.value;
+    }
 }
