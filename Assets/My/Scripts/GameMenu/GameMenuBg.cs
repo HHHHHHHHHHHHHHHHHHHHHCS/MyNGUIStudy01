c781932c1699f8b9d16f330069d9038982c1a772
HCS <464962683@qq.com>
2018-04-29 23:17:52 +0800

    控制两个界面的切换

---
+++ Assets/My/Scripts/GameMenu/GameMenuBg.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuBg : MonoBehaviour
+{
+    private TweenPosition exitAnim;
+
+    private void Awake()
+    {
+        var startButton = transform.Find("StartGameButton").gameObject;
+        var optionButton = transform.Find("OptionButton").gameObject;
+        var exitButton = transform.Find("ExitButton").gameObject;
+
+        exitAnim = transform.GetComponent<TweenPosition>();
+
+        UIEventListener.Get(optionButton).onClick += GotoGameOptionBg;
+        exitAnim.AddOnFinished(() => { gameObject.SetActive(false); });
+    }
+
+
+    private void GotoGameOptionBg(GameObject newGo)
+    {
+        exitAnim.PlayForward();
+        transform.parent.Find("GameOptionBg").GetComponent<GameOptionBg>()
+            .EnterAnim();
+    }
+}
--- Assets/My/Scripts/GameMenu/GameOptionBg.cs
+++ Assets/My/Scripts/GameMenu/GameOptionBg.cs
@@ -18,18 +18,24 @@ public enum ControlType
 
 
 
-public class GameSetting : MonoBehaviour
+public class GameOptionBg : MonoBehaviour
 {
-    public  float volume = 1;
-    public  GameGrade gameGrade = GameGrade.Esay;
-    public  ControlType controlType = ControlType.Keyboard;
-    public  bool isFullScreen = false;
+    public float volume = 1;
+    public GameGrade gameGrade = GameGrade.Esay;
+    public ControlType controlType = ControlType.Keyboard;
+    public bool isFullScreen = false;
 
 
     private UISlider volumeSlider;
     private UIPopupList gameGradeList;
     private UIPopupList gameControllerList;
     private UIToggle isFullScreenToggle;
+    private UIButton completeOptionButton;
+
+
+    private TweenPosition exitAnim_pos;
+    private TweenScale exitAnim_sacle;
+
 
     private void Awake()
     {
@@ -37,12 +43,16 @@ public class GameSetting : MonoBehaviour
         gameGradeList = transform.Find("GameGradeLabel/GameGradeList").GetComponent<UIPopupList>();
         gameControllerList = transform.Find("GameControllerLabel/GameControllerList").GetComponent<UIPopupList>();
         isFullScreenToggle = transform.Find("IsFullScreenLabel/IsFullToggle").GetComponent<UIToggle>();
+        completeOptionButton = transform.Find("CompleteOptionButton").GetComponent<UIButton>();
 
+        exitAnim_pos = transform.GetComponent<TweenPosition>();
+        exitAnim_sacle = transform.GetComponent<TweenScale>();
 
         EventDelegate.Add(volumeSlider.onChange, OnVolumeSliderChanged);
         EventDelegate.Add(gameGradeList.onChange, OnGameGradeChanged);
         EventDelegate.Add(gameControllerList.onChange, OnGameControlChanged);
         EventDelegate.Add(isFullScreenToggle.onChange, OnIsFullChanged);
+        EventDelegate.Add(completeOptionButton.onClick, OnCompleteOptionClick);
 
     }
 
@@ -94,5 +104,25 @@ public class GameSetting : MonoBehaviour
     {
         Debug.Log("OnIsFullChanged:" + UIToggle.current.value);
         isFullScreen = UIToggle.current.value;
+    }
+
+    private void OnCompleteOptionClick()
+    {
+        Debug.Log("OnCompleteOptionClick:Save");
+        exitAnim_pos.PlayReverse();
+        exitAnim_sacle.PlayReverse();
+    }
+
+    public void EnterAnim()
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(_EnterAnim());
+    }
+
+    private IEnumerator _EnterAnim()
+    {
+        yield return 0;
+        exitAnim_pos.PlayForward();
+        exitAnim_sacle.PlayForward();
     }
 }
