88eef3a191bc22d79e0c78112208d2bede833aef
HCS <464962683@qq.com>
2018-05-02 23:45:36 +0800

    隐藏测试

---
+++ Assets/My/Scripts/Test02/Test02.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test02 : MonoBehaviour
+{
+    public Material mat;
+    public Vector4 vec4;
+
+    private Material defMat;
+
+	// Use this for initialization
+	void Start ()
+    {
+        var atlas = GetComponent<UISprite>().atlas;
+        defMat = atlas.spriteMaterial;
+        atlas.spriteMaterial = mat;
+}
+
+    private void Update()
+    {
+        //mat.SetVector("_ClipRange0", new Vector4(vec4.x, vec4.y, Mathf.Sin(vec4.z), Mathf.Cos(vec4.w)));
+
+    }
+
+    void OnDestroy()
+    {
+        GetComponent<UISprite>().atlas.spriteMaterial = defMat;
+    }
+}
