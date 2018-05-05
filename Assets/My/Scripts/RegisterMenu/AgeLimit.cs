06fcfd5f77427529bb998d9db65aef81aa70281a
HCS <464962683@qq.com>
2018-05-05 22:44:29 +0800

    注册菜单的年龄和姓名限制  还有一些限制

---
+++ Assets/My/Scripts/RegisterMenu/AgeLimit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgeLimit : MonoBehaviour
+{
+    private UIInput ageInput;
+
+    private void Awake()
+    {
+        ageInput = GetComponent<UIInput>();
+        EventDelegate.Add(ageInput.onSubmit, OnAgeValueSubmit);
+    }
+
+    private void OnAgeValueSubmit()
+    {
+        string val = ageInput.value;
+        int age = int.Parse(val);
+
+        age = Mathf.Clamp(age, 18, 120);
+        ageInput.value = age.ToString();
+    }
+}
---
+++ Assets/My/Scripts/RegisterMenu/NameLimit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class NameLimit : MonoBehaviour
+{
+    private UIInput nameInput;
+
+    private void Awake()
+    {
+        nameInput = GetComponent<UIInput>();
+        EventDelegate.Add(nameInput.onSubmit, OnNameValueSubmit);
+    }
+
+    private void OnNameValueSubmit()
+    {
+        Regex regChina = new Regex("^[\u4e00-\u9fa5]+$");
+        Regex regEnglish = new Regex("^[a-zA-Z]+$");
+        string str = nameInput.value;
+        if (regEnglish.IsMatch(str))
+        {
+            Debug.Log(str);
+            Debug.Log("是英文");
+        }
+        else if (regChina.IsMatch(str))
+        {
+            Debug.Log("是中文");
+        }
+        else
+        {
+            Debug.Log("输入类型错误");
+        }
+    }
+}
