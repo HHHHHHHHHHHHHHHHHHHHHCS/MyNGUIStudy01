008cad81809f65c64e30cb25e9b7b028fdf267ce
HCS <464962683@qq.com>
2018-05-07 23:38:50 +0800

    滚动文字列表的制作

---
+++ Assets/My/Scripts/ScrollBarText/ScrollTextList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTextList : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.GetComponent<UITextList>()
+                .Add( Random.Range(0, 123456789).ToString());
+        }
+    }
+}
