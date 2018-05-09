216470594ea1b9d59ba7a9ee7e35226d47c1117c
HCS <464962683@qq.com>
2018-05-09 23:48:43 +0800

    基础的聊天的制作

---
+++ Assets/My/Scripts/ScrollBarText/MyChatInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyChatInput : MonoBehaviour
+{
+    [SerializeField]
+    private UITextList textList;
+
+    private UIInput uiInput;
+
+    private void Awake()
+    {
+        uiInput = GetComponent<UIInput>();
+        //UIEventListener.Get(gameObject).onSubmit += OnChatSubmit;
+        EventDelegate.Add(uiInput.onSubmit, OnChatSubmit);
+    }
+
+
+    private void OnChatSubmit()
+    {
+        string chat = uiInput.value;
+        textList.Add(chat);
+        uiInput.value = "";
+    }
+}
