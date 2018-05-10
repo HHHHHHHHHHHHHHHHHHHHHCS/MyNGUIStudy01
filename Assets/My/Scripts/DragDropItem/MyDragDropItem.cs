b6b39bc2a894c7fffc16de47addc0316f50a962a
HCS <464962683@qq.com>
2018-05-11 00:20:13 +0800

    背包功能的机制制作（未完成）

---
+++ Assets/My/Scripts/DragDropItem/MyDragDropItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDragDropItem : UIDragDropItem
+{
+    protected override void OnDragDropRelease(GameObject surface)
+    {
+        Debug.Log(surface);
+        base.OnDragDropRelease(surface);
+    }
+}
